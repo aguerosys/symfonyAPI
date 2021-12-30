5b37d85e7ea955714f7f840f9cce5a0ad38f8731
Aguero Brian Ezequiel <aguerolinkk@gmail.com>
2021-12-30 17:45:52 -0300

    Add logger, add library controller

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController{
@@ -11,8 +14,10 @@ class LibraryController extends AbstractController{
 
     #[Route('/library/list', name: 'library_list')]
 
-    public function list(){
+    public function list(Request $request, LoggerInterface $logger){
 
+        $title = $request->get('title', 'Alegria');
+        $logger->info('List action called 2');
         $response = new JsonResponse();
         $response->setData([
             'success' => true,
@@ -23,6 +28,10 @@ class LibraryController extends AbstractController{
             [
                 'id' => 2,
                 'title' => 'Clean Code'
+            ],
+            [
+                'id' => 3,
+                'title' => $title
             ]
 
 
