8c5988f8085060e673f25945f188ed8848bff353
Aguero Brian Ezequiel <aguerolinkk@gmail.com>
2022-01-05 12:31:10 -0300

    Se agrego el endpoint post para crear los books

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -3,9 +3,12 @@
     namespace App\Controller\Api;
 
     use App\Repository\BookRepository;
+    use Doctrine\ORM\EntityManagerInterface;
     use FOS\RestBundle\Controller\AbstractFOSRestController;
     use FOS\RestBundle\Controller\Annotations as Rest;
+    use Symfony\Component\HttpFoundation\JsonResponse;
     use Symfony\Component\HttpFoundation\Request;
+    use App\Entity\Book;
 
     class BooksController extends AbstractFOSRestController{
 
@@ -18,6 +21,32 @@
         public function getActions(BookRepository $bookRepository)
         {
             return $bookRepository->findAll();
+        }
+
+        /**
+            * @Rest\Post(path="api/books")
+            * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
+        */
+
+        public function postActions(EntityManagerInterface $em, Request $request )
+        {
+            $book = new Book();
+            $response = new JsonResponse();
+            $title = $request->get('title', null);
+            if (empty($title)){
+                $response->setData([
+                    'success' => false,
+                    'error' => 'title cannot be empty',
+                    'data' => null
+
+                ]);
+                return $response;
+            }
+            $book->setTitle($title);
+            $book->setImage('p');
+            $em->persist($book);
+            $em->flush();
+            return $book;
         }
     }
 
